0c945319a1931ac6ad589f384766d4144a96d73e
Ashish777Dev <ashishvasavafox43@gmail.com>
2026-05-09 00:12:11 +0530

    refactor : add a proper path of context folder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import { ProductContextProvider } from "./components/context/ProductContext";
+import { ProductContextProvider } from "./context/ProductContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
